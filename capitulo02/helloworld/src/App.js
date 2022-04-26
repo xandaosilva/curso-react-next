54797ffcd96ee17babfb63ffd54b56131481ded0
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-04-26 18:10:47 -0300

    State com arrays e objects

--- capitulo02/helloworld/src/App.js
+++ capitulo02/helloworld/src/App.js
@@ -1,32 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
 
 class App extends Component{
   state = {
-    message: "Edit <code>src/App.js</code> and save to reload.",
-    counter: 0
+    posts: [
+      {
+        id: 1,
+        title: 'Livro 1',
+        body: 'Conteúdo do livro 1'
+      },
+      {
+        id: 2,
+        title: 'Livro 2',
+        body: 'Conteúdo do livro 2'
+      },
+      {
+        id: 3,
+        title: 'Livro 3',
+        body: 'Conteúdo do livro 3'
+      }
+    ]
   };
 
-  handlePClick = () => this.setState({ message: "Edite <code>src/App.js</code> e salve para recarregar ." });
-
-  handleAClick = (event) => {
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState({counter: counter + 1})
-  }
-
   render(){
-    const {message, counter} = this.state;
+    const { posts } = this.state;
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClick}>{message} {counter}</p>
-          <a onClick={this.handleAClick} className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
-        </header>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
